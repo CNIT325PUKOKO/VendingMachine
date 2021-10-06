caef2fc56e671fecee3e30227ecd579edc145c93
amadeo3 <91677967+amadeo3@users.noreply.github.com>
2021-10-06 14:37:33 -0400

    Add files via upload

--- Machine.java
+++ Machine.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Machine here.
+ * Machine contains arrays of slot objects and controls the interaction between slots, products, and the controller
  *
  * @author Adam, Kayla, Zephyn
- * @version 1.0
+ * @version 3.0
  */
 import java.lang.Math;
 public class Machine implements Machinable
@@ -16,7 +16,6 @@ public class Machine implements Machinable
     private Slot[][] medSlots;
     private Slot[] bigSlots;
     double cashTotal;
-    private Dispense dispenser = new Dispense();
 
     /**
      * Constructor for objects of class Machine
@@ -33,30 +32,42 @@ public class Machine implements Machinable
         createSlots();
     }
 
+    /**
+     * Creates the slots based on the size of the constants set in the constructor
+     *
+     * @param  no parameters
+     * @return   void
+     */
     private void createSlots()
     {
         int ascii = 65;
         for (int i = 0; i < SMALLSIZE; i++)
         {
-            smallSlots[i] = new Slot((char)ascii + Integer.toString((i+1)));
+            smallSlots[i] = new Slot((char)ascii + Integer.toString((i)));
         }
         ascii++;
         for (int r = 0; r < MEDROWS; r++)
         {
             for (int c= 0; c < MEDCOLS; c++)
             {
-                medSlots[r][c] = new Slot((char)ascii + Integer.toString(c+1));
+                medSlots[r][c] = new Slot((char)ascii + Integer.toString(c));
                 
             }
             ascii++;
         }
-        ascii++;
+        //ascii++;
         for (int i = 0; i < BIGSIZE; i++)
         {
-            bigSlots[i] = new Slot((char)ascii + Integer.toString((i+1)));
+            bigSlots[i] = new Slot((char)ascii + Integer.toString((i)));
         }
     }
     
+    /**
+     * Fills the slots with products
+     *
+     * @param  String array for names, Size array for sizes, double array for prices
+     * @return   void
+     */
     public void populateProducts(String[] n, Size[] sizes, double[] p)
     {
         int i = 0;
@@ -91,38 +102,16 @@ public class Machine implements Machinable
         }
     }
     
-    public void printMachine()
-    {
-        for(Slot s : smallSlots)
-        {
-            System.out.println(s.getSlotNum());
-        }
-        for(int r = 0; r < MEDROWS; r++)
-        {
-            for(Slot s : medSlots[r])
-            {
-                System.out.println(s.getSlotNum());
-            }
-        }
-        for(Slot s : bigSlots)
-        {
-            System.out.println(s.getSlotNum());
-        }
-    }
-    
     /**
      * Checks to see if the amount entered is correct
      *
-     * @param  no parameters
+     * @param  String for the slot number, String for the payment amount
      * @return   String
      */
     public String comparePrice(String slotNum, String payAmount)
     {
         double paid = Double.parseDouble(payAmount);
         double price = getPrice(slotNum);
-        int quantity = checkQuantity(slotNum);
-        if(quantity <= 0)
-            return("-1");
         if(paid > price)
         {
             double change = paid - price;
@@ -144,57 +133,73 @@ public class Machine implements Machinable
     /**
      * Checks to see if the selected item is stocked
      *
-     * @param  no parameters
-     * @return   String
+     * @param  String for the slot number
+     * @return   int
      */
     public int checkQuantity(String sn)
     {
         String slotNumber = sn;
-        int column = Integer.parseInt(slotNumber.substring(1,2)) - 1;
+        int column = Integer.parseInt(slotNumber.substring(1,2));
         char bigRow = (char)(66 + MEDROWS);
-        int quantity;
-        int row = (int)slotNumber.charAt(0) - 65;
-        if(slotNumber.charAt(0) == 'A')
-        {
-            quantity = smallSlots[column].getQuantity();
+        int quantity = -1;
+        int row = (int)slotNumber.charAt(0) - 66;
+        try{
+            if(slotNumber.charAt(0) == 'A')
+            {
+                quantity = smallSlots[column].getQuantity();
+            }
+            else if(slotNumber.charAt(0) == bigRow)
+            {
+                quantity = bigSlots[column].getQuantity();
+            }
+            else
+            {
+                quantity = medSlots[row][column].getQuantity();
+            }
         }
-        else if(slotNumber.charAt(0) == bigRow)
+        catch(Exception e)
         {
-            quantity = bigSlots[column].getQuantity();
-        }
-        else
-        {
-            quantity = medSlots[row][column].getQuantity();
+            return -2;
         }
         return quantity;
-    }
-
+    }  
     
-    
+    /**
+     * Gets the price for the item in the slot an returns it
+     *
+     * @param  String for the slot number
+     * @return   double
+     */
     public double getPrice(String sn)
     {
         String slotNumber = sn;
-        int column = Integer.parseInt(slotNumber.substring(1,2)) - 1;
+        int column = Integer.parseInt(slotNumber.substring(1,2));
         char bigRow = (char)(66 + MEDROWS);
-        double price;
-        int row = (int)slotNumber.charAt(0) - 65;
-        if(slotNumber.charAt(0) == 'A')
-        {
-            price = smallSlots[column].getPrice();
-        }
-        else if(slotNumber.charAt(0) == bigRow)
-        {
-            price = bigSlots[column].getPrice();
-        }
+        double price = -1;
+        int row = (int)slotNumber.charAt(0) - 66;
+        int quantity = -2;
+        if(checkQuantity(slotNumber) <= 0)
+            return(-2); 
         else
+            quantity = checkQuantity(slotNumber);
+        try{
+            if(slotNumber.charAt(0) == 'A')
+            {
+                price = smallSlots[column].getPrice();
+            }
+            else if(slotNumber.charAt(0) == bigRow)
+            {
+                price = bigSlots[column].getPrice();
+            }
+            else
+            {
+                price = medSlots[row][column].getPrice();
+            }
+        }
+        catch(Exception e)
         {
-            price = medSlots[row][column].getPrice();
+            return -1;
         }
         return price;
     }
-    
-    public void dispenseItem(String slotNum)
-    {
-        dispenser.dispenseProd(slotNum);
-    } 
 }
