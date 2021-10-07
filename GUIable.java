d5715856513ef5b7c55e8af7b9e208068f548e6c
amadeo3 <91677967+amadeo3@users.noreply.github.com>
2021-10-07 10:36:38 -0400

    Add files via upload

--- GUIable.java
+++ GUIable.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of interface GUIable here.
+ * Interface for GUI class
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Jack, Michael
+ * @version 1.0
  */
 public interface GUIable
 {
